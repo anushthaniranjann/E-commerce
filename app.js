d8443ecc40dc17d5acabce7714e490b14f43f8de
Anushtha <anushthaniranjan@gmail.com>
2021-06-17 23:46:52 +0530

    Adding express

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const http = require('http');
+const express = require('express');
 
-const routes = require('./routes');
+const app = express();
 
-const server = http.createServer(routes);
+app.use((req, res, next) => {
+    console.log('in middleware')
+    next(); //allows req to conitnue to next middleware
+});
+
+app.use((req, res, next) => {
+    console.log('Tired')
+});
+
+const server = http.createServer(app);
 
 server.listen(2000);
