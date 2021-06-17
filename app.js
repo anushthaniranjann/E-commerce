f054bb4a71d1f7d352fcf0375b4996442224adc6
Anushtha <anushthaniranjan@gmail.com>
2021-06-18 00:43:22 +0530

    Display product

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
-//const http = require('http')
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-//const server = http.createServer(app)
+
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(express.urlencoded({ extended: false })); Alternative for body-parser
 
 const port = process.env.PORT || 3000
 
 app.use('/add-product', (req, res, next) => {
-    console.log('Tired');
+    console.log('Gud Night');
     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</form>');
 });
 
-app.use('/product', (req, res, next) => {
+app.post('/product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 })
 
 app.use('/', (req, res, next) => {
-    console.log('Tired');
     res.send('<h1>Hello</h1>');
 });
-
-// server.listen(port, () => {
-//     console.log(`Server is up on port ${port}`)
-// })
 
 app.listen(port, port, () => {
     console.log(`Server is up on port ${port}`)
