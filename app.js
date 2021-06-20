575632653896454eea2e0b1167c29f974661b92d
Anushtha <anushthaniranjan@gmail.com>
2021-06-20 21:17:59 +0530

    Added routes

--- app.js
+++ app.js
@@ -3,24 +3,17 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(express.urlencoded({ extended: false })); Alternative for body-parser
 
 const port = process.env.PORT || 3000
 
-app.use('/add-product', (req, res, next) => {
-    console.log('Gud Night');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</form>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-})
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello</h1>');
-});
 
 app.listen(port, port, () => {
     console.log(`Server is up on port ${port}`)
