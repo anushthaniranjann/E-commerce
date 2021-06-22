3e61b2d61f53140728f3dc11517643ff0603790a
Anushtha <anushthaniranjan@gmail.com>
2021-06-22 14:01:12 +0530

    Render html files

--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
+// /admin/add-product =>  Get
 router.get('/add-product', (req, res, next) => {
     console.log('Product added');
     res.send('<form action="/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</form>');
 });
 
+// /admin/add-product => POST
 router.post('/product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
--- routes/shop.js
+++ routes/shop.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.send('<h1>Hello</h1>');
+    res.sendFile('/');
 });
 
 module.exports = router;
