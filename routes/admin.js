491d1dda853b99ed9ae8c3cc88d32b24e9e16026
Anushtha <anushthaniranjan@gmail.com>
2021-06-23 22:15:26 +0530

    Modified path and display error

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,15 @@
+const path = require('path');
+
 const express = require('express');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 // /admin/add-product =>  Get
 router.get('/add-product', (req, res, next) => {
     console.log('Product added');
-    res.send('<form action="/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</form>');
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 // /admin/add-product => POST
